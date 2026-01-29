018d3ee9343ee3b299150a8a44d27f589c6ba60a
49fd6m92vc-code <49fd6m92vc@privaterelay.appleid.com>
2026-01-28 20:51:26 -0500

    Add array of photo filenames to photos.js

---
+++ photos.js
@@ -0,0 +1,23 @@
+const photos = [
+  "WMRN-2026-012_1970s_AM-Control-Room_01.jpg",
+  "WMRN-2026-012_1970s_FM-Control-Room_01.jpg",
+  "WMRN-2026-012_1970s_Paul-Van-House_In-Studio_01.jpg",
+  "WMRN-2026-012_1970s_Paul-Van-House_In-Studio_02.jpg",
+  "WMRN-2026-012_1970s_Paul-Van-House_In-Studio_03.jpg",
+  "WMRN-2026-012_1970s_Stereo-Marion-Studios_01.jpg",
+  "WMRN-2026-012_1970s_WMRN-Antenna-Tower_01.jpg",
+  "WMRN-2026-012_1970s_WMRN-Remote-and-News-Van_01.jpg",
+  "WMRN-2026-012_1978_Studios-After-Blizzard_01.jpg",
+
+  "WMRN-2026-012_1979_Peanut-Push_Awards-Ceremony_Charlie-Evers-at-Microphone_01.jpg",
+  "WMRN-2026-012_1979_Peanut-Push_Bob-George_Larry-Mathiot_Susan-Westfall_01.jpg",
+  "WMRN-2026-012_1979_Peanut-Push_Bob-George_Playing-National-Anthem-on-Saw_01.jpg",
+  "WMRN-2026-012_1979_Peanut-Push_Group_Larry-Mathiot_Charlie-Evers_Randy-Roeder_01.jpg",
+  "WMRN-2026-012_1979_Peanut-Push_Larry-Mathiot_01.jpg",
+
+  "WMRN-2026-012_1980_Karen-Sharp-Wedding_Becky-Van-House_Paul-Michaels_Susan-Westfall_Jeff-Ruth_01 (1).jpg",
+  "WMRN-2026-012_1980_Karen-Sharp-Wedding_Becky-Van-House_Paul-Michaels_S提醒-Westfall_Jeff-Ruth_01.jpg".replace("提醒-", ""),
+  "WMRN-2026-012_1980_Paul-Van-House_Dracula-Costume_Orbit_01.jpg",
+
+  "WMRN-2026-012_Orbit_Show_Susan-Westfall_Paul-Van-House_Studio-Costume_01.jpg"
+];
